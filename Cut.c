6ccb31657f53f5f2c4fe230724031aa31116be6b
shamanpatel <shamanpatel74@gmail.com>
2024-03-17 15:49:42 -0700

    String Parsing with Delimiter for Multiple Ranges

--- Cut.c
+++ Cut.c
@@ -17,41 +17,36 @@ void cut_bytes(const char *input, int start, int end) {
 }
 
 int main() {
-    char input[50];
-    int start;
-    int end;
+    char input[100];
+    int start, end;
+    char ranges[50]; 
 
     printf("Enter the input string: ");
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
-    printf("From index: ");
-    scanf("%d", &start);
+    printf("Enter the ranges from start1-end1,start2-end2,...): ");
+    fgets(ranges, sizeof(ranges), stdin);
 
-    printf("To index: ");
-    scanf("%d", &end);
+    // Replace '\n' with '\0' to remove newline character
+    ranges[strcspn(ranges, "\n")] = '\0';
 
-    if (start == end) {
-        printf("Input String: %s\n", input);
-        printf("%d:%c\n", start, input[start]);
-    } else {
-        printf("Input String: %s\n", input);
-        printf("Range(%d-%d): ", start, end);
-        cut_bytes(input, start, end);
+    // strtok() is used to delimit the string into substrings. Here the delimiter is ','.
+    char *token = strtok(ranges, ",");
+    while (token != NULL) {
+        // sscanf is uded to parse the token ranges.
+        sscanf(token, "%d-%d", &start, &end);
+
+        if (start == end) {
+            printf("Input String: %s\n", input);
+            printf("%d:%c\n", start, input[start]);
+        } else {
+            printf("Input String: %s\n", input);
+            printf("Range(%d-%d): ", start, end);
+            cut_bytes(input, start, end);
+        }
+
+        token = strtok(NULL, ","); // Move to the next token
     }
 
     return 0;
 }
-// shaman@devdsk:~/workspace/coding_practice$ gcc Cut.c -o Cut
-// shaman@devdsk:~/workspace/coding_practice$ ./Cut
-// Enter the input string: Sachi Sagarkumar Patel
-// From index: 0
-// To index: 20
-// Input String: Sachi Sagarkumar Patel
-// Range(0-20): Sachi Sagarkumar Pate
-// shaman@devdsk:~/workspace/coding_practice$ gcc Cut.c -o Cut
-// shaman@devdsk:~/workspace/coding_practice$ ./Cut
-// Enter the input string: Sachi Patel
-// From index: 0
-// To index: 0
-// Input String: Sachi Patel
-// 0:S
